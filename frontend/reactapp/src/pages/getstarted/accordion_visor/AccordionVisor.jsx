540b3062ebc1fe202432ee5b564bf53c2773a234
Rohit Kulkarni <kulkarni.rohitva@gmail.com>
2022-09-15 22:58:05 -0400

    stuff fixed (mostly), still need to fix scaling issues

--- frontend/reactapp/src/pages/getstarted/accordion_visor/AccordionVisor.jsx
+++ frontend/reactapp/src/pages/getstarted/accordion_visor/AccordionVisor.jsx
@@ -3,39 +3,39 @@ import './accordionvisor.css';
 import Accordion from 'react-bootstrap/Accordion';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Covdths from './covdths/Covdths';
-import Dths from './dths/Dths';
-import Medchars from './medchars/Medchars';
-import Podst from './podst/Podst';
+import Covdths from './get_started_comps/covdths/Covdths';
+import Dths from './get_started_comps/dths/Dths';
+import Medchars from './get_started_comps/medchars/Medchars';
+import Podst from './get_started_comps/podst/Podst';
 
 
 function BasicExample() {
     return (
     <div class="bootstrap-ui">
-      <Accordion defaultActiveKey="0">
+      <Accordion>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>View the Covdths Data!</Accordion.Header>
           <Accordion.Body>
             <Covdths />
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>View the Dths Data!</Accordion.Header>
           <Accordion.Body>
             <Dths />
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="2">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>View the Medchars Data!</Accordion.Header>
           <Accordion.Body>
             <Medchars />
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="3">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>View the Podst Data!</Accordion.Header>
           <Accordion.Body>
             <Podst />
           </Accordion.Body>
