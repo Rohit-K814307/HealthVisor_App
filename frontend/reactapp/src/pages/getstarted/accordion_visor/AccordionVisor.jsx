494f11d833a736275af19a51281937786974cc96
Rohit Kulkarni <kulkarni.rohitva@gmail.com>
2022-09-16 19:50:16 -0400

    accordion fixed and frontend working

--- frontend/reactapp/src/pages/getstarted/accordion_visor/AccordionVisor.jsx
+++ frontend/reactapp/src/pages/getstarted/accordion_visor/AccordionVisor.jsx
@@ -1,49 +1,87 @@
 import React from 'react';
 import './accordionvisor.css';
-import Accordion from 'react-bootstrap/Accordion';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Covdths from './get_started_comps/covdths/Covdths';
 import Dths from './get_started_comps/dths/Dths';
 import Medchars from './get_started_comps/medchars/Medchars';
 import Podst from './get_started_comps/podst/Podst';
 
-
-function BasicExample() {
+// Accordion component
+const Accordion = ({ title, children }) => {
+    const [isOpen, setIsOpen] = React.useState(false)
+    const [height, setHeight] = React.useState(0)
+    const bodyRef = React.useRef(null)
+    
+    // Get the collapsed body height
+    React.useEffect(() => {
+      const elementHeight = bodyRef.current.clientHeight
+      setHeight(elementHeight)
+    }, [])
+     
+    // inline style
+    const collapse = {
+      height: 0,
+      transition: "height .3s ease"
+    }
+  
+    const show = {
+      height: `${height}px`,
+      transition: "height .3s ease"
+    }
+  
     return (
-    <div class="bootstrap-ui">
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>View the Covdths Data!</Accordion.Header>
-          <Accordion.Body>
-            <Covdths />
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>View the Dths Data!</Accordion.Header>
-          <Accordion.Body>
-            <Dths />
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>View the Medchars Data!</Accordion.Header>
-          <Accordion.Body>
-            <Medchars />
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="3">
-          <Accordion.Header>View the Podst Data!</Accordion.Header>
-          <Accordion.Body>
-            <Podst />
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </div>
-    );
+      <div className="card">
+        <div className="card-header">
+          <h2 className="mb-0">
+            <button
+              className="btn btn-link"
+              type="button"
+              aria-expanded={isOpen}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {title}
+            </button>
+          </h2>
+        </div>
+        <div style={isOpen ? show : collapse}>
+          <div className="card-body" ref={bodyRef}>
+            {children}
+          </div>
+        </div>
+      </div>
+    )
   }
   
-  export default BasicExample;
+  function AccordionVisor() {
+    return (
+      <div className="visor-holder">
+        <div className="accordion">
 
+          <Accordion title="Covdths">
+            <div>
+              <Covdths />
+            </div>
+          </Accordion>
+
+          <Accordion title="Dths">
+            <div>
+              <Dths />
+            </div>
+          </Accordion>
+
+          <Accordion title="Medchars">
+            <div>
+              <Medchars />
+            </div>
+          </Accordion>
+
+          <Accordion title="Podst">
+            <div>
+              <Podst />
+            </div>
+          </Accordion>
+        </div>
+      </div>
+    )
+  }
+
+  export default AccordionVisor;
