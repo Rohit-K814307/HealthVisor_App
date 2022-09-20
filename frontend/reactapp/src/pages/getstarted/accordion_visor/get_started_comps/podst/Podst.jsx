8719f4829e68069dec8f4c5a60348c20d37455b2
Rohit Kulkarni <kulkarni.rohitva@gmail.com>
2022-09-19 22:42:16 -0400

    updated yearlist for podst

--- frontend/reactapp/src/pages/getstarted/accordion_visor/get_started_comps/podst/Podst.jsx
+++ frontend/reactapp/src/pages/getstarted/accordion_visor/get_started_comps/podst/Podst.jsx
@@ -41,7 +41,7 @@ export default function Podst() {
                         <select id="years" onChange={handleChangeYear}>
                             <option value="all" selected="selected">All</option>
                             <option value="2019">2019</option>
-                            <option value="2018">2018</option>
+                            <option value="2020">2020</option>
                         </select>
                     </form>
                 </div>
